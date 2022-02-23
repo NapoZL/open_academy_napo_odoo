cfd9c8446a2c0b23342575a2f849d79a19fdc458
NapoZL <52946545+NapoZL@users.noreply.github.com>
2022-02-23 11:33:31 -0600

    Domains

---
+++ open_academy/models/course.py
@@ -0,0 +1,17 @@
+ #-*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class Course(models.Model):
+     _name = 'open_academy.course'
+     _description = 'Course'
+     _responsible_id = 'example'
+     _session_ids = 'exaxmple'
+
+     name = fields.Char(string='Course')
+     title = fields.Char(string='Title')
+     description = fields.Text(string='Description')
+     responsible_id = fields.Many2one('res.users',
+        ondelete='set null', string="Responsible", index=True)
+     session_ids = fields.One2many(
+        'open_academy.session', 'course_id', string="Sessions")
